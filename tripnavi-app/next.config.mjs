15dad0277ecbc06d3a39f0935192d86f98763a24
takemi853 <amk0k2takemi@gmail.com>
2024-09-10 10:59:26 +0900

    update

--- tripnavi-app/next.config.mjs
+++ tripnavi-app/next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    api: {
-      bodyParser: {
-        sizeLimit: '1mb', // 必要に応じてサイズ制限を設定
-      },
-    },
+
   };
   
   export default nextConfig;
